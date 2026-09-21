2f98e5959dabbec321c6fc70accffa11975a41e9
sinem <sinem.karagoz@student.rocva.nl>
2026-09-21 19:23:28 +0200

    Voeg planeten route toe

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/planeten', function () {
+    return [
+        "Uranus",
+        "Jupiter",
+        "Mars",
+        "Aarde",
+        "Saturnus",
+        "Pluto",
+        "Neptunus",
+        "Venus"
+    ];
+});
