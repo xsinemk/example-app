70ed01c54bac7e9ca90e0a071d4fa13baebb355a
sinem <sinem.karagoz@student.rocva.nl>
2026-09-22 12:24:26 +0200

    Add planets route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/planeten', function () {
+Route::get('/planets', function () {
     return [
         "Uranus",
         "Jupiter",
